f3a64566f6e44c6ab7cb9629e936346846ce2a94
Pavel Merekin <merekin33@gmail.com>
2024-10-01 13:47:39 +0800

    portfolio slider ready

--- script.js
+++ script.js
@@ -40,3 +40,42 @@ hardSkillsTop.addEventListener('click', () =>
 softSkillsTop.addEventListener('click', () =>
   toggleSkills(softSkillsBtn, softSkillsDivider, softSkillsList, '380px')
 );
+
+const slides = document.querySelectorAll('.slides img');
+let slideIndex = 0;
+let intervalId = null;
+
+initializeSlider();
+function initializeSlider() {
+  if (slides.length > 0) {
+    slides[slideIndex].classList.add('displaySlide');
+    intervalId = setInterval(nextSlide, 5000);
+  }
+}
+
+function showSlide(index) {
+  if (index >= slides.length) {
+    slideIndex = 0;
+  } else if (index < 0) {
+    slideIndex = slides.length - 1;
+  }
+
+  slides.forEach((slide) => {
+    slide.classList.remove('displaySlide');
+  });
+  slides[slideIndex].classList.add('displaySlide');
+}
+
+function prevSlide() {
+  clearInterval(intervalId);
+  slideIndex--;
+  showSlide(slideIndex);
+  intervalId = setInterval(nextSlide, 5000);
+}
+
+function nextSlide() {
+  clearInterval(intervalId);
+  slideIndex++;
+  showSlide(slideIndex);
+  intervalId = setInterval(nextSlide, 5000);
+}
